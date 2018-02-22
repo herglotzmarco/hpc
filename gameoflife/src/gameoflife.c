a5fc301d4aa5bc531a11d10e9dedc49229f92f61
marco.herglotz <herglotz.m@gmail.com>
2018-02-22 15:33:30 +0100

    Implemented cycle (there is a bug but I can't find it...

--- gameoflife/src/gameoflife.c
+++ gameoflife/src/gameoflife.c
@@ -46,10 +46,80 @@ void printField() {
 	printf("\n\n-----------------------------------\n");
 }
 
-void cycle() {
+int checkIndex(int current, int i) {
+	int bounds = i >= 0 && i < sizeX * sizeY;
+	int rowBoundsRight = current % sizeX == sizeX - 1;
+	if (rowBoundsRight) {
+		rowBoundsRight = rowBoundsRight && i != current + 1;
+	} else {
+		rowBoundsRight = 1;
+	}
 
+	int rowBoundsLeft = current % sizeX == 0;
+	if (rowBoundsLeft) {
+		rowBoundsLeft = rowBoundsLeft && i != current - 1;
+	} else {
+		rowBoundsLeft = 1;
+	}
+
+	return bounds && rowBoundsLeft && rowBoundsRight;
 }
 
+int countNeighbours(int index) {
+	int count = 0;
+
+// left and right
+	if (checkIndex(index, index - 1) && getField(index - 1))
+		count++;
+	if (checkIndex(index, index + 1) && getField(index + 1))
+		count++;
+
+// above
+	int row = index - sizeX;
+	if (checkIndex(row, row - 1) && getField(row - 1))
+		count++;
+	if (checkIndex(row, row) && getField(row))
+		count++;
+	if (checkIndex(row, row + 1) && getField(row + 1))
+		count++;
+
+// below
+	row = index + sizeX;
+	if (checkIndex(row, row - 1) && getField(row - 1))
+		count++;
+	if (checkIndex(row, row) && getField(row))
+		count++;
+	if (checkIndex(row, row + 1) && getField(row + 1))
+		count++;
+
+	return count;
+}
+
+int computeFromNeighbourCount(int neighbours, int current) {
+	// dead cell
+	if (!current && neighbours == 3)
+		return 1; // come to life
+	if (!current)
+		return 0; // stay dead
+
+	// living cells
+	if (neighbours < 2)
+		return 0; // die of starvation
+	if (neighbours >= 4)
+		return 0; // die of overpopulation
+	return 1; // else stay alive
+}
+
+void cycle() {
+	for (int i = 0; i < sizeX * sizeY; i++) {
+		int neighbours = countNeighbours(i);
+		if (computeFromNeighbourCount(neighbours, getField(i))) {
+			setField(i);
+		} else {
+			unsetField(i);
+		}
+	}
+}
 
 int main(void) {
 	initField(sizeX, sizeY);
@@ -60,7 +130,7 @@ int main(void) {
 	setField(22);
 	printField();
 
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 1; i++) {
 		cycle();
 		printField();
 	}
